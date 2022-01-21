8b24e0ccfefa652a041bd609c5d3a01437f68624
Adrian R <rod21691@uvg.edu.gt>
2022-01-21 17:57:19 -0600

    Finalizacion de interfaz y clase Radio con metodos implementados

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,58 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		boolean salir = false;
-		int opcion = 0;
+		boolean running = true;
+		int option = 0;
 		Radio radio = new Radio();
-		
-		while (salir == false) {
+		int slot;
+		int freqAM;
+		float freqFM;
+
+		while (running == true) {
 			System.out.println(
-					"1.Prender radio\n2.Cambiar tipo de frecuencia\n3.Avanzar en el dial de emisoras\n4.Guardar emisora\n5.Seleccionar emisora\n6.Apagar radio");
-			opcion = Integer.parseInt(input.nextLine());
-			switch (opcion) {
+					"1.Prender/Apagar radio\n2.Cambiar tipo de frecuencia\n3.Avanzar en el dial de emisoras\n4.Guardar emisora\n5.Seleccionar emisora");
+			option = Integer.parseInt(input.nextLine());
+			switch (option) {
 			case 1:
-				
+				radio.switchButton();
+				System.out.println(radio.getStatus());
 				break;
 			case 2:
-
+				radio.changeMode();
+				System.out.println(radio.getActualMode());
 				break;
 			case 3:
-
+				radio.moveForward();
+				if (radio.getActualMode() == 0) {
+					System.out.println(radio.getActualFreqAM());
+				} else {
+					System.out.println(radio.getActualFreqFM());
+				}
 				break;
 			case 4:
+				System.out.println("Ingrese el espacio donde desea guardar la emisora (1-12)");
+				slot = Integer.parseInt(input.nextLine());
+				if (radio.getActualMode() == 0) {
 
+					System.out.println("Ingrese la frequencia AM");
+					freqAM = Integer.parseInt(input.nextLine());
+					radio.saveInAM(slot, freqAM);
+
+				} else {
+
+					System.out.println("Ingrese la frequencia FM");
+					freqFM = Float.parseFloat(input.nextLine());
+					radio.saveInFM(slot, freqFM);
+				}
 				break;
 			case 5:
-
-				break;
-			case 6:
-
+				System.out.println("Ingrese el espacio donde se encuentra su emisora de eleccion (1-12)");
+				slot = Integer.parseInt(input.nextLine());
+				if (radio.getActualMode() == 0) {
+					System.out.println(radio.getSavedFreqAM(slot));
+				} else {
+					System.out.println(radio.getSavedFreqFM(slot));
+				}
 				break;
 			}
 		}
--- src/Radio.java
+++ src/Radio.java
@@ -1,72 +1,108 @@
 
 public class Radio implements IRadio {
-	private boolean encendido;
-	private int modoActual;
-	
-	
+	private boolean turnedOn = false;
+	private int radioMode = 0; // 0 = AM 1 = FM
+	private int[] savedAM = new int[12]; // sube en multiplos de 10
+	private double[] savedFM = new double[12]; // sube en multiplos de 0.2
+	private int actualFreqAM = 530;
+	private double actualFreqFM = 87.9;
+
 	@Override
 	public boolean getStatus() {
-		// TODO Auto-generated method stub
-		return false;
+
+		return turnedOn;
 	}
+
 	@Override
 	public void switchButton() {
-		// TODO Auto-generated method stub
-		
+		if (getStatus() == false) {
+			turnedOn = true;
+		} else {
+			turnedOn = false;
+		}
+
 	}
+
 	@Override
 	public void changeMode() {
-		// TODO Auto-generated method stub
-		
+		if (getActualMode() == 0) {
+			radioMode = 1;
+		} else {
+			radioMode = 0;
+		}
+
 	}
+
 	@Override
 	public int getActualMode() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return radioMode;
 	}
+
 	@Override
 	public void saveInAM(int slot, int freq) {
-		// TODO Auto-generated method stub
-		
+		savedAM[slot - 1] = freq;
+
 	}
+
 	@Override
 	public int getSavedFreqAM(int slot) {
-		// TODO Auto-generated method stub
-		return 0;
+		actualFreqAM = savedAM[slot - 1];
+		return savedAM[slot - 1];
 	}
+
 	@Override
 	public void saveInFM(int slot, double freq) {
-		// TODO Auto-generated method stub
-		
+		savedFM[slot - 1] = freq;
+
 	}
+
 	@Override
 	public double getSavedFreqFM(int slot) {
-		// TODO Auto-generated method stub
-		return 0;
+		actualFreqFM = savedFM[slot - 1];
+		return savedFM[slot - 1];
 	}
+
 	@Override
 	public int getActualFreqAM() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return actualFreqAM;
 	}
+
 	@Override
 	public double getActualFreqFM() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return actualFreqFM;
 	}
+
 	@Override
 	public void moveForward() {
-		// TODO Auto-generated method stub
-		
+
+		if (getActualMode() == 0) {
+
+			if (actualFreqAM >= 1610) {
+				actualFreqAM = 530;
+			} else {
+				actualFreqAM += 10;
+			}
+		} else {
+			if (actualFreqFM >= 107.9) {
+				actualFreqFM = 87.9;
+			} else {
+				actualFreqFM += 0.2;
+			}
+		}
 	}
+
 	@Override
 	public void moveBackward() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void seek() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
